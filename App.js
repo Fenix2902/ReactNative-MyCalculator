a7b3255f3bd6d573892f5f9cc7dd5cb00b80f2e0
Danna Lorena Salinas Sánchez <130772842+dannalorenassalinassanchez@users.noreply.github.com>
2023-08-30 08:43:20 -0500

    30/08/2023 se realiza la instalacion de react-native-paper y se empieza a implementar en los textinput y botones, se crea metodo para las operaciones y limpieza

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import { useState, useEffect } from "react";
 import {
-  StyleSheet,
   Text,
   View,
-  TextInput,
-  Button,
-  TouchableOpacity,
-  Touchable,
 } from "react-native";
+import { TextInput, Button } from "react-native-paper";
 import { Banner } from "./src/components/Banner";
 import { Footer } from "./src/components/Footer";
 import { styles, myimage } from "./assets/styles/Styles1";
@@ -21,6 +17,28 @@ export default function App() {
 
   let mymess = "Se implementa correo, telefono";
   let image = "calculadora.png";
+
+  let calculate = (operator) => {
+    let mresult = 0;
+    let mvalue1 = parseFloat(value1);
+    let mvalue2 = parseFloat(value2);
+    switch (operator) {
+      case "+":
+        mresult = mvalue1 + mvalue2;
+        break;
+      case "-":
+        mresult = mvalue1 - mvalue2;
+        break;
+      case "*":
+        mresult = mvalue1 * mvalue2;
+        break;
+      case "/":
+        mresult = mvalue1 / mvalue2;
+        break;
+    }
+    setResult(mresult); //actualizando la vble de estado result
+  };
+
   return (
     <View style={styles.container}>
       <Banner image={image} />
@@ -36,22 +54,57 @@ export default function App() {
         <Text>Calculadora</Text>
         <Text>Numero 1</Text>
         <TextInput
-          placeholder="Ingrese valor 1"
+          label="Valor Uno"
           onChangeText={(value1) => setValue1(value1)}
           value={value1}
-          style={styles.tinputs}
+          style={{ backgroundColor: "powderblue" }}
+          left={<TextInput.Icon icon="account" />}
         />
         <Text>Numero 2</Text>
         <TextInput
-          placeholder="Ingrese valor 2"
+          label="Valor Dos"
           onChangeText={(value2) => setValue2(value2)}
           value={value2}
-          style={styles.tinputs}
+          style={{ backgroundColor: "powderblue" }}
+          right={<TextInput.Icon icon="account" />}
         />
-        <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity style={{ backgroundColor: "red" }}>
-            <Text>+</Text>
-          </TouchableOpacity>
+        <View style={{ flexDirection: "row", marginTop: 15 }}>
+          <Button icon="plus" mode="contained" onPress={() => calculate("+")}>
+            Suma
+          </Button>
+          <Button icon="minus" mode="contained" onPress={() => calculate("-")}>
+            Resta
+          </Button>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 15 }}>
+          <Button
+            icon="multiplication"
+            mode="contained"
+            onPress={() => calculate("*")}
+          >
+            Multiplicación
+          </Button>
+          <Button
+            icon="slash-forward"
+            mode="contained"
+            onPress={() => calculate("/")}
+          >
+            División
+          </Button>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 15 }}>
+          <Button
+            buttonColor="red"
+            icon="close-circle-outline"
+            mode="contained"
+            onPress={() => {
+              setValue1("")
+              setValue2("")
+              setResult("")
+            }}
+          >
+            Limpiar
+          </Button>
         </View>
       </View>
 
@@ -64,7 +117,8 @@ export default function App() {
           justifyContent: "center",
         }}
       >
-        <Text>Cuerpo</Text>
+        <Text>Resultado</Text>
+        <Text>{parseFloat(result).toFixed(1)}</Text>
       </View>
       <Footer text={mymess} />
     </View>
